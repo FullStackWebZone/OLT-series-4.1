e0add550b85908f62c937d9e7eb9ea095e8e53eb
FullStackWebZone <71539844+FullStackWebZone@users.noreply.github.com>
2021-02-06 21:41:47 +0530

    OLT-series-4.1

---
+++ Create an Instagram Double Tab Like Functionality/script.js
@@ -0,0 +1,21 @@
+const clickArea = document.querySelector('.inner-div');
+const clickCount = document.querySelector('.clickCount');
+
+let likeCount = 0;
+{}
+// {<i class="fa fa-heart" aria-hidden="true"></i>}
+const showHeart = (e) =>{
+	const heart = document.createElement('i');
+	heart.classList.add('fa');
+	heart.classList.add('fa-heart');
+	clickArea.appendChild(heart);
+
+	setTimeout(()=>{
+		heart.remove();
+	},800);
+}
+clickArea.addEventListener('dblclick', (e) => {
+	clickCount.innerHTML = ++likeCount;
+
+	showHeart(e);
+});
